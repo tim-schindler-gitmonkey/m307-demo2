cef93fe7c67ed1f64bd18a06d21120364014f723
Tim <tim.schindler@bbz-cfp.ch>
2024-11-20 17:55:21 +0000

    version 1

--- config.js
+++ config.js
@@ -4,8 +4,10 @@ import pg from "pg";
 const { Pool } = pg;
 import cookieParser from "cookie-parser";
 import multer from "multer";
-const upload = multer({ dest: "public/uploads/" });
 import sessions from "express-session";
+import bcrypt from "bcrypt";
+
+const upload = multer({ dest: "public/uploads/" });
 
 export function createApp(dbconfig) {
   const app = express();
@@ -30,6 +32,50 @@ export function createApp(dbconfig) {
   );
 
   app.locals.pool = pool;
+
+  app.get("/register", function (req, res) {
+    res.render("register");
+  });
+
+  app.post("/register", function (req, res) {
+    var password = bcrypt.hashSync(req.body.password, 10);
+
+    pool.query(
+      "INSERT INTO users (name, passwort, hobbies, age) VALUES ($1, $2, $3, $4)",
+      [req.body.name, password, req.body.hobbies, req.body.age],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        res.redirect("/login");
+      }
+    );
+  });
+
+  app.get("/login", function (req, res) {
+    res.render("login");
+  });
+
+  app.post("/login", function (req, res) {
+    pool.query(
+      "SELECT * FROM users WHERE name = $1",
+      [req.body.name],
+      (error, result) => {
+        if (error) {
+          console.log(error);
+        }
+        if (
+          result.rows.length > 0 &&
+          bcrypt.compareSync(req.body.password, result.rows[0].passwort)
+        ) {
+          req.session.userid = result.rows[0].id;
+          res.redirect("/");
+        } else {
+          res.redirect("/login");
+        }
+      }
+    );
+  });
 
   return app;
 }
--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-import { createApp } from "./config.js";
+import { createApp, upload } from "./config.js";
 
 const app = createApp({
   user: "autumn_star_7622",
-  host: "168.119.168.41",
+  host: "bbz.cloud",
   database: "demo",
   password: "uaioysdfjoysfdf",
-  port: 18324,
+  port: 30211,
 });
 
 /* Startseite */
@@ -16,6 +16,23 @@ app.get("/", async function (req, res) {
 app.get("/impressum", async function (req, res) {
   res.render("impressum", {});
 });
+
+app.get("/register", async function (req, res) {
+  res.render("register", {});
+});
+
+app.get("/register-pfp", async function (req, res) {
+  res.render("register-pfp", {});
+});
+
+function myFunction() {
+  var x = document.getElementById("password");
+  if (x.type === "password") {
+    x.type = "text";
+  } else {
+    x.type = "password";
+  }
+}
 
 /* Wichtig! Diese Zeilen müssen immer am Schluss der Website stehen! */
 app.listen(3010, () => {
